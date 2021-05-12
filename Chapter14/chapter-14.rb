43320b133302de6389e1671bdc4cc856ada615a7
Jeremy Evans <code@jeremyevans.net>
2021-05-11 19:47:58 -0700

    Add code for chapter 14

---
+++ Chapter14/chapter-14.rb
@@ -0,0 +1,34 @@
+### The Database Is Key
+
+## Treating the database as not just dumb storage
+
+# SELECT first_name || ' ' || last_name FROM names;
+
+# --
+
+# SELECT first_name, last_name FROM names
+
+# --
+
+"#{row[:first_name]} #{row[:last_name]}"
+
+## Choosing the model layer
+
+tracks = 10
+Album.where("num_tracks > ?", tracks).to_a
+
+# --
+
+tracks = 10
+Album.where{num_tracks > tracks}.all
+
+## Handling database and model errors
+
+model_instance.save
+# => model or false
+
+# --
+
+model_instance.save
+# => model
+# or raise Sequel::ValidationFailed
